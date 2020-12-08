3e5f3d5f451d5f45d4573f751fb5c0b543921cb3
Ethan Mabry <47231729+ethanmabry@users.noreply.github.com>
2020-12-08 17:15:52 -0500

    Create SquaringProgram.cpp

---
+++ SquaringProgram/SquaringProgram.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  
+  while (i < 10)
+  {
+  
+  std::cout << i << " " << square << std::endl;
+  i++;
+  square = i*i;
+
+  }
+  
+  
+  
+  
+  
+  
+  
+}
